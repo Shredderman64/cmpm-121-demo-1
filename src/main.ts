1a60251717a19b6aa836d16ecb962a48df314205
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-06 23:26:54 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "This is a button 🙃";
+app.append(button);
