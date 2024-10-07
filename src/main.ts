a220bffe71dc2a0ae56abf974768344e26374c54
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-07 13:27:51 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My spectacular game";
+const gameName = "WOW such a good game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+const message = document.createElement("div");
+message.innerHTML = `${counter} sarcastic comments`;
+app.append(message)
+
 const button = document.createElement("button");
-button.innerHTML = "This is a button 🙃";
+button.innerHTML = "🙃";
+button.addEventListener("click", () => {
+    message.innerHTML = `${++counter} sarcastic comments`;
+})
 app.append(button);
