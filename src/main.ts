39e9fb959c685e78f61e7468f375120d0d6343fa
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-07 20:42:58 -0700

    Added upgrade button and growth rate

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,22 @@ button.innerHTML = "🙃";
 button.addEventListener("click", makeComment);
 app.append(button);
 
+const upgrade = document.createElement("button");
+upgrade.innerHTML = "10 bob";
+app.append(upgrade);
+
 function makeComment() {
   message.innerHTML = `${++counter} sarcastic comments`;
 }
 
 const PER_SECOND = 1000;
+const growth_rate = 0;
 let zero = performance.now();
 requestAnimationFrame((t) => update(t));
 
 function update(timestamp: number) {
   const elapsed = timestamp - zero;
-  counter += elapsed / PER_SECOND;
+  counter += elapsed * growth_rate / PER_SECOND;
   message.innerHTML = `${Math.trunc(counter)} sarcastic comments`;
   zero = timestamp;
   requestAnimationFrame((t) => update(t));
